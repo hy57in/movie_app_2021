cf8fdb931b60113543e0cd01498a4636cc6d4649
HyojinKim <gywls00100@gmail.com>
2021-06-29 16:52:23 +0900

    Make Calulator by state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 
-function Potato({ fav }) {
-  return <h1>I like {fav}</h1>;
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello React!</h1>
-      <Potato fav="potato1" />
-      <Potato fav="potato2" />
-      <Potato fav="potato3" />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  minus = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;

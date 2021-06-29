c639075efd5db84c7f04ecfc40c1717a9e23d399
HyojinKim <gywls00100@gmail.com>
2021-06-29 14:16:10 +0900

    Make function and use props to send data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+function Potato({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
+
 function App() {
   return (
     <div>
       <h1>Hello React!</h1>
+      <Potato fav="potato1" />
+      <Potato fav="potato2" />
+      <Potato fav="potato3" />
     </div>
   );
 }
